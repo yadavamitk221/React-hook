3281377de318d9f9d7f828f32f50e24936931cd8
Amitkumar Yadav (Capgemini America Inc) <v-amiyadav@microsoft.com>
2023-07-22 12:03:51 +0530

    first commit

---
+++ src/Components/Reset.js
@@ -0,0 +1,31 @@
+import useLocalStorageForEmail from "./useLocalStorageForEmail";
+
+export default function Reset(){
+
+    //const [email,setEmail] = useState("");
+
+    //destructuring {email,setEmail} from  useLocalStorageForEmail hook
+   const {email,setEmail} = useLocalStorageForEmail();
+
+    return(
+        <>
+        <h3>Reset Password for</h3>
+      <input
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          // Logic to send a new password follows
+        }}
+      >
+        Submit
+      </button>      
+      <br />
+        </>
+    )
+}
